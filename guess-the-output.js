74e834b913ea8e97e5610f9a12c1f1b325901698
Bharat Dua <bharatdua13@gmail.com>
2025-05-15 17:43:57 +0530

    Solve : JS output question 5 - while loop

--- guess-the-output.js
+++ guess-the-output.js
@@ -19,3 +19,35 @@ console.log(a); // 5
 let a = [1, 2, 3, 4, 5];
 console.log(a.slice(2, 4)); // [3, 4]
 // Explanation: The slice() method returns a shallow copy of a portion of an array. It takes two parameters: (start, end), the **start** index is included, but the **end** index is not.So, `a.slice(2, 4)` means take items from index 2 **up to** (but not including) index 4.If the array is `[1, 2, 3, 4, 5]`, this will return `[3, 4]` (the items at index 2 and 3).
+
+//* 5.
+
+var a = 1;
+var b = 0;
+while (a <= 3) {
+  a++;
+  b += a * 2;
+  console.log(b); // 4,10,18
+}
+/*
+Iteration cycle :-
+iteration 1 :- a = 1, b = 0
+while (a <= 3) { // true
+  a++; // a = 2
+  b += a * 2; // b = 0 + 2 * 2 = 4
+  console.log(b); // 4
+}
+
+iteration 2 :- a = 2, b = 4
+while (a <= 3) { // true
+    a++; // a = 3
+    b += a * 2; // b = 4 + 3 * 2 = 10
+    console.log(b); // 4,10
+
+iteration 3 :- a = 3, b = 10 
+    while (a <= 3) { // true 
+    a++; // a = 4 
+    b += a * 2; // b = 10 + 4 * 2 = 18
+console.log(b); // 4,10,18
+
+*/
