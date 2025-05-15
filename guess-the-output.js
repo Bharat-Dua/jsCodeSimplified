9974eecaeab125ba2e8e21c320cd6946826f1624
Bharat Dua <bharatdua13@gmail.com>
2025-05-15 17:18:13 +0530

    Solve: JS output question 2 - NaN

--- guess-the-output.js
+++ guess-the-output.js
@@ -4,3 +4,7 @@ const obj1 = { first: 20, second: 30, first: 50 };
 console.log(obj1);
 // Output: { first: 50, second: 30 }
 // Explanation: The last value given to the first property replaces the one before it. This happens because JavaScript objects can change, and if you set the same property more than once, the last one wins. That’s why the result is { first: 50, second: 30 }.
+
+//* 2.
+console.log(typeof NaN); // number
+// Explanation: NaN is a number, so typeof NaN returns "number".
