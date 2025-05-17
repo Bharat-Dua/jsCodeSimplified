559189c506dbad52fa28e0b0b54e7b9defd066a2
Bharat Dua <bharatdua13@gmail.com>
2025-05-17 14:03:03 +0530

    Solve: JS output question 39 - addition assignment operator

--- guess-the-output.js
+++ guess-the-output.js
@@ -283,3 +283,10 @@ while (count < 10) {
   console.log(count); // 0,1,2,3,4,5,6,7,8,9
   count++;
 }
+
+//* 39.
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+  sum += i;
+}
+console.log(sum); // 15
